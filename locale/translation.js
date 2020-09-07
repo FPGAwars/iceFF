17e979be07bd8a518247bdc6e2377282fd09f650
Obijuan <juan@iearobotics.com>
2020-09-07 08:21:13 +0200

    Initial Translation into spanish

--- locale/translation.js
+++ locale/translation.js
@@ -7,4 +7,16 @@
 // 1. Open the PO file with Poedit
 // 2. Press "Update" to update from sources
 
-
+gettext('FFs');
+gettext('House');
+gettext('DFF');
+gettext('D Flip-flop (verilog implementation)');
+gettext('Parameter: Initial value');
+gettext('System clock');
+gettext('Input data');
+gettext('Output');
+gettext('# D Flip-Flop  \n\nIt stores the input data that arrives at cycle n  \nIts output is shown in the cycle n+1');
+gettext('DFF');
+gettext('D Flip-flop');
+gettext('TESTs');
+gettext('DFF');
